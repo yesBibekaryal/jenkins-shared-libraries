c1bc65172c61630ef839461a2e8c6ceef6dcbbdd
Sandeep Pokhrel <35886764+sananddev@users.noreply.github.com>
2025-06-07 17:39:24 +1000

    Update update_k8s_manifests.groovy

--- vars/update_k8s_manifests.groovy
+++ vars/update_k8s_manifests.groovy
@@ -47,7 +47,7 @@ def call(Map config = [:]) {
                 git commit -m "Update image tags to ${imageTag} and ensure correct domain [ci skip]"
                 
                 # Set up credentials for push
-                git remote set-url origin https://\${GIT_USERNAME}:\${GIT_PASSWORD}@github.com/LondheShubham153/tws-e-commerce-app.git
+                git remote set-url origin https://\${GIT_USERNAME}:\${GIT_PASSWORD}@github.com/sananddev/tws-e-commerce-app.git
                 git push origin HEAD:\${GIT_BRANCH}
             fi
         """
